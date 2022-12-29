a95ae3267d977d352edba5b9f7c3393cc7e2e3fe
Tedr <avpokrov@gmail.com>
2022-12-29 08:32:34 +0300

    Work routing

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ require('dotenv').config();
 const {PORT = 3000} = process.env;
 const app = express();
 console.log(PORT);
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+// mongoose.connect('mongodb://localhost:27017/mestodb', {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false
+// });
 
-app.use('/users', require('./routes/users'));
+const router = require('./router/index.js');
+
+app.use(router);
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
