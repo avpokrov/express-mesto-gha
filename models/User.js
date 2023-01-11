9e98615c7a257ac8fea69b48524c6d792c3d8e60
Tedr <avpokrov@gmail.com>
2023-01-11 17:06:15 +0300

    feat: start 14 sprint

--- models/User.js
+++ models/User.js
@@ -1,21 +1,39 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Поле email должно быть заполнено!'],
+    unique: true,
+    validate: {
+      validator: (email) => { validator.isEmail(email); },
+      message: 'Указан не корректный email',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Пароль не может быть пустым!'],
+    select: true,
+  },
   name: {
     type: String,
-    required: true,
+    required: false,
     minlength: 2,
     maxlength: 30,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
+    required: false,
     minlength: 2,
     maxlength: 30,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: true,
+    required: false,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
 });
 
